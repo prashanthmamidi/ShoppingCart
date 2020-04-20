a7322e0bbf0e17b62df3cadd5cf6082bf35b02a3
Prashanth Mamidi <prashanth.mamidi@gmail.com>
2020-04-20 23:54:42 +0100

    Step2: Apply Offers

--- src/main/scala/org/hmrc/ShoppingCart.scala
+++ src/main/scala/org/hmrc/ShoppingCart.scala
@@ -1,16 +1,26 @@
 package org.hmrc
 
 class ShoppingCart {
-  def compute(shoppingList: List[String]): String = {
+  def checkout(shoppingList: List[String]): String = {
+    val totalCost = shoppingList.map(getPrice).sum.toDouble
 
-    val totalCost = shoppingList.map {
-      case "Apple" => 60
-      case "Orange" => 25
-      case item => throw new IllegalArgumentException(s"Item ${item} is invalid")
-    }.sum.toDouble
+    val offers = offerPriceFor(shoppingList, "Apple", 2) +
+      offerPriceFor(shoppingList, "Orange", 3)
 
-    s"£${totalCost/100}"
+    s"£${(totalCost - offers) / 100}"
   }
 
+  private def getPrice(item: String) = {
+    item match {
+      case "Apple" => 60
+      case "Orange" => 25
+      case item => throw new IllegalArgumentException(s"Item $item is invalid")
+    }
+  }
+
+  private def offerPriceFor(shoppingList: List[String], fruitType: String, offer: Int) = {
+    val fruitCount = shoppingList.count(_ == fruitType)
+    (fruitCount / offer) * getPrice(fruitType)
+  }
 
 }
--- src/test/scala/org/hmrc/ShoppingCartSpec.scala
+++ src/test/scala/org/hmrc/ShoppingCartSpec.scala
@@ -8,33 +8,65 @@ class ShoppingCartSpec extends AnyFlatSpec with Matchers {
   val shoppingCart = new ShoppingCart()
 
   "shopping cart" should "calculate the price of items in the basket" in {
-    val result = shoppingCart.compute(List("Apple", "Apple", "Orange", "Apple"))
+    val result = shoppingCart.checkout(List("Apple", "Apple", "Orange", "Apple"))
 
-    result shouldEqual "£2.05"
+    result shouldEqual "£1.45"
   }
 
   it should "calculate the price of basket, given basket has only Apple" in {
-    val result = shoppingCart.compute(List("Apple"));
+    val result = shoppingCart.checkout(List("Apple"));
 
     result shouldEqual "£0.6"
   }
 
   it should "calculate the price of basket, given basket has only Orange" in {
-    val result = shoppingCart.compute(List("Orange"));
+    val result = shoppingCart.checkout(List("Orange"));
 
     result shouldEqual "£0.25"
   }
 
   it should "return 0 if basket is empty" in {
-    val result = shoppingCart.compute(List())
+    val result = shoppingCart.checkout(List())
     result shouldEqual "£0.0"
   }
 
   it should "throw exception if basket has invalid item" in {
     val exception = intercept[IllegalArgumentException] {
-      shoppingCart.compute(List("Apple", "Banana", "Orange"))
+      shoppingCart.checkout(List("Apple", "Banana", "Orange"))
     }
     exception.getMessage shouldBe "Item Banana is invalid"
   }
+
+  "shopping cart with offers" should "apply buy one get one offer, given basket has 2 Apples" in {
+    val result = shoppingCart.checkout(List("Apple", "Apple", "Orange")) // Apple cost -> 0.60, Orange cost -> 0.25
+
+    result shouldEqual "£0.85"
+  }
+
+  it should "apply buy one get one offer, given basket has 5 Apples" in {
+    val result = shoppingCart.checkout(List("Apple", "Apple", "Apple", "Apple", "Apple", "Orange")) // Apple cost -> 1.80, Orange cost -> 0.25
+
+    result shouldEqual "£2.05"  // cost of 3 apples and 1 orange
+  }
+
+  it should "apply 3 for 2 offer, given basket has 3 Oranges" in {
+    val result = shoppingCart.checkout(List("Apple", "Orange", "Orange", "Orange")) // Apple cost -> 0.60, Orange cost -> 0.50
+
+    result shouldEqual "£1.1"  // cost of 3 apples and 1 orange
+  }
+
+  it should "apply 3 for 2 offer, given basket has 5 Oranges" in {
+    val result = shoppingCart.checkout(List("Apple", "Orange", "Orange", "Orange",  "Orange",  "Orange")) // Apple cost -> 0.60, Orange cost -> 1.00
+
+    result shouldEqual "£1.6"  // cost of 3 apples and 1 orange
+  }
+
+  it should "apply buy one get one offer and 3 for 2 offer, given basket has 5 Apples and 4 Oranges" in {
+    val result = shoppingCart.checkout(List("Apple", "Apple", "Apple", "Apple", "Apple", "Orange", "Orange", "Orange", "Orange")) // Apple cost -> 1.80, Orange cost -> 0.75
+
+    result shouldEqual "£2.55"  // cost of 3 apples and 1 orange
+  }
+
+
 
 }
